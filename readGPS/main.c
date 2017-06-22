931ffe1ae8bce95e98e0b6655ee2b42d1e6d8ff3
20161104590 <shihaoranqq.com@foxmail.com>
2017-06-22 11:37:08 +0800

    重写了代码

--- readGPS/main.c
+++ readGPS/main.c
@@ -7,50 +7,29 @@
 //
 
 #include <stdio.h>
-#define N 2
-typedef
-struct {
-    char name[6];
-    int time;
-    char a[1];
-    int latitude;
-    char b[1];
-    int longitude;
-    char c[1];
-    int rate;
-    int course;
-    int date;
-    char d[3];
-} gps;
-void savegps(gps [],char *);
-void readgps(char *);
-int main(int argc, const char * argv[]) {
+
+int main(int argc, const char * argv[]){
+    // insert code here...
+    FILE *fr;
+    FILE *fw;
+    char str1[70];
+    char str2[70];
+    char lat[8],time[6];
     int i;
-    gps gps1[N];
-    for(i=0;i<N;i++)
-    {
-        printf("输入第%d个gps的名字:",i+1);
-        gets(gps1[i].name);
-        printf("输入第%d个gps的UTC时间:",i+1);
-        scanf("%d",&gps1[i].time);
-        printf("输入第%d个gps的定位状态:",i+1);
-        scanf("%s",gps1[i].a);
-        printf("输入第%d个gps的纬度:",i+1);
-        scanf("%d",&gps1[i].latitude);
-        printf("输入第%d个gps的纬度半球:",i+1);
-        scanf("%s",gps1[i].b);
-        printf("输入第%d个gps的经度:",i+1);
-        scanf("%d",&gps1[i].longitude);
-        printf("输入第%d个gps的经度半球:",i+1);
-        scanf("%s",gps1[i].c);
-        printf("输入第%d个gps的地面速率:",i+1);
-        scanf("%d",&gps1[i].rate);
-        printf("输入第%d个gps的地面航向:",i+1);
-        scanf("%d",&gps1[i].course);
-        printf("输入第%d个gps的UTC日期:",i+1);
-        scanf("%d",&gps1[i].date);
-        printf("输入第%d个gps的异或和:",i+1);
-        scanf("%s",gps1[i].d);
-        getchar();
-    }
-    savegps(gps1,);
+    fr=fopen("//Users//a20161104590//Desktop//readGPS//GPS170510.log","r+");
+    fw=fopen("//Users//a20161104590//Desktop//readGPS//readGPS//output","w");
+    fscanf(fr,"%s%s",str1,str2);
+
+    for(i=0;i<6;i++)
+        time[i]=str1[i+7];
+    time[6]='\0';
+    for(i=0;i<8;i++)
+        lat[i]=str1[i+16];
+    lat[8]='\0';
+    printf("时间：%s\n",time);
+    printf("纬度：%s\n",lat);
+    fprintf(fw,"纬度：%s\n",time);
+    fprintf(fw,"时间：%s\n",lat);
+    fclose(fr);
+    return 0;
+}
