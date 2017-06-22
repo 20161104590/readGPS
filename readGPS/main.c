9f22d1d59fd8c77bd5456cf06610c9a0981eed67
20161104590 <shihaoranqq.com@foxmail.com>
2017-06-22 11:57:27 +0800

    修改了一个错误，加入fprintf语句
    将数组扩大了一个单位，防止容量不够

--- readGPS/main.c
+++ readGPS/main.c
@@ -14,22 +14,26 @@ int main(int argc, const char * argv[]){
     FILE *fw;
     char str1[70];
     char str2[70];
-    char lat[8],time[6];
+    char lat[9],time[7],date[7];
     int i;
     fr=fopen("//Users//a20161104590//Desktop//readGPS//GPS170510.log","r+");
     fw=fopen("//Users//a20161104590//Desktop//readGPS//readGPS//output","w");
     fscanf(fr,"%s%s",str1,str2);
-
     for(i=0;i<6;i++)
         time[i]=str1[i+7];
     time[6]='\0';
+    printf("时间：%s\n",time);
+    fprintf(fw,"时间：%s\n",time);
     for(i=0;i<8;i++)
         lat[i]=str1[i+16];
     lat[8]='\0';
-    printf("时间：%s\n",time);
     printf("纬度：%s\n",lat);
-    fprintf(fw,"纬度：%s\n",time);
-    fprintf(fw,"时间：%s\n",lat);
+    fprintf(fw,"纬度：%s\n",lat);
+    for(i=0;i<6;i++)
+        date[i]=str1[i+51];
+    date[6]='\0';
+    printf("日期：%s\n",date);
+    fprintf(fw,"日期：%s\n",date);
     fclose(fr);
     return 0;
 }
